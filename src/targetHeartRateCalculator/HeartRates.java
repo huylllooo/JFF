8cbe26788e004f198743034a0f1d4a98806b7c64
Huy M. Nguyen <nmhuy14@gmail.com>
2016-06-20 23:07:55 +0900

    Created project

---
+++ src/targetHeartRateCalculator/HeartRates.java
@@ -0,0 +1,13 @@
+package targetHeartRateCalculator;
+
+public class HeartRates {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
